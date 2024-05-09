d0c112129bd42d02d8f8d1b9a5566985fc97aa24
degyuhijhvfr <ssjcofiahershey@hotmail.com>
2024-05-10 06:35:45 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-function canJump(nums) {
-  let lastPos = nums.length - 1;
-  for (let i = nums.length - 2; i >= 0; i--) {
-    if (i + nums[i] >= lastPos) lastPos = i;
+function isValidBST(root) {
+  let prev = null;
+  return inorder(root);
+  function inorder(node) {
+    if (!node) return true;
+    if (!inorder(node.left)) return false;
+    if (prev !== null && node.val <= prev) return false;
+    prev = node.val;
+    return inorder(node.right);
   }
-  return lastPos === 0;
 }
