111e9b9bbb36fccf5035a7cd08d926be3319672b
degyuhijhvfr <ssjcofiahershey@hotmail.com>
2024-05-11 14:27:36 +0700

    update aptos project commit

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const countOccurrences = (arr, value) =>
-  arr.reduce((a, v) => (v === value ? a + 1 : a), 0);
+function minMeetingRoomsII(intervals) {
+  const startTimes = intervals
+    .map((interval) => interval[0])
+    .sort((a, b) => a - b);
+  const endTimes = intervals
+    .map((interval) => interval[1])
+    .sort((a, b) => a - b);
+  let rooms = 0;
+  let endIdx = 0;
+  for (let i = 0; i < startTimes.length; i++) {
+    if (startTimes[i] < endTimes[endIdx]) rooms++;
+    else endIdx++;
+  }
+  return rooms;
+}
