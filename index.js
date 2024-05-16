45f3a42ae665d7a7882300d891663d4c9aafc650
degyuhijhvfr <ssjcofiahershey@hotmail.com>
2024-05-16 07:23:53 +0700

    start

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-function bubbleSort(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
-    }
-  }
-  return arr;
+function flattenArray(arr) {
+  return arr.reduce(
+    (acc, curr) => acc.concat(Array.isArray(curr) ? flattenArray(curr) : curr),
+    [],
+  );
 }
